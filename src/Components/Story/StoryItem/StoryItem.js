64e1fe17ad7f518ec4b050069c510195cc2367f2
batuhanbaybas <b.baybas@gmail.com>
2022-02-18 23:14:57 +0300

    sory update

--- src/Components/Story/StoryItem/StoryItem.js
+++ src/Components/Story/StoryItem/StoryItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const StoryItem = () => {
     return (
-        <div className='flex flex-col justify-center py-4 px-2'>
+        <div className='flex flex-col justify-center p-4'>
             <img src='https://cdn.pixabay.com/photo/2019/03/21/20/29/eyewear-4071870__340.jpg'
             alt='resim' className='rounded-full object-cover w-16 h-16' />
             <span className='text-xs pt-2'>Kullanıcı ismi</span>
