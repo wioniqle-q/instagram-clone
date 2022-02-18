66c6ae20b7c2c538dbcec8eebac9ba11cb78bf2b
batuhanbaybas <b.baybas@gmail.com>
2022-02-19 02:21:12 +0300

    profil page

--- frontend/src/Components/Posts/Posts.js
+++ frontend/src/Components/Posts/Posts.js
@@ -17,6 +17,28 @@ const Posts = () => {
                     TAGGED
                 </span>
             </div>
+            <div className='pt-5 flex flex-wrap'>
+
+                <img src='https://cdn.pixabay.com/photo/2022/02/15/07/27/travel-7014427_960_720.jpg' alt='resim'
+                    className='w-1/3 p-2  object-cover'
+                />
+                <img src='http://cdn.webtekno.com/custom/images/panaromik/hd-wallpapers-animals-technology-cat.jpg' alt='resim'
+                    className='w-1/3 p-2  object-cover '
+                />
+                <img src='https://cdn.pixabay.com/photo/2022/02/15/07/27/travel-7014427_960_720.jpg' alt='resim'
+                    className='w-1/3 p-2  object-cover'
+                />
+                <img src='https://cdn.pixabay.com/photo/2022/02/15/07/27/travel-7014427_960_720.jpg' alt='resim'
+                    className='w-1/3 p-2  object-cover'
+                />
+                <img src='https://cdn.pixabay.com/photo/2022/02/15/07/27/travel-7014427_960_720.jpg' alt='resim'
+                    className='w-1/3 p-2  object-cover'
+                />
+
+                <img src='https://cdn.pixabay.com/photo/2022/02/15/07/27/travel-7014427_960_720.jpg' alt='resim'
+                    className='w-1/3 p-2  object-cover'
+                />
+            </div>
         </div>
     )
 }
