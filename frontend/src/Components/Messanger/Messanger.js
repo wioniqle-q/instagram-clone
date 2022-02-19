fb2fc8d6b389715c8be1d0b1ed722d1c9341c701
batuhanbaybas <b.baybas@gmail.com>
2022-02-20 01:44:36 +0300

    chatroom ready

--- frontend/src/Components/Messanger/Messanger.js
+++ frontend/src/Components/Messanger/Messanger.js
@@ -12,13 +12,40 @@ const Messanger = () => {
                     <span className='text-xs font-semibold px-2'>kullanıcı Adı </span>
                 </div>
             </div>
-            <div className='mt-5'>
-                <div className='px-10'>
-                    
+            <div className='mt-5 overflow-auto h-[72vh]'>
+                <div className='px-5 mb-3 w-2/3 float-right'>
+                    <p className="border rounded-xl p-3 flex flex-start z-0">
+                        Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
+                        metinlerdir. Lorem Ipsum,
+                        adı bilinmeyen bir matbaacının bir hurufat numune kitabı
+                        oluşturmak üzere bir yazı galerisini alarak karıştırdığı
+                        1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.
+                        Beşyüz yıl boyunca varlığını sürdürmekle kalmamış,
+                        aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda
+                        Lorem Ipsum pasajları da içeren Letraset yapraklarının
+                        yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren
+                        masaüstü yayıncılık yazılımları ile popüler olmuştur.
+                        Neden Kullanırız?
+                    </p>
+                </div>
+                <div className='px-5 mb-3 w-2/3 float-left'>
+                    <p className="border rounded-xl p-3 flex flex-start">
+                        Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
+                        metinlerdir. Lorem Ipsum,
+                        adı bilinmeyen bir matbaacının bir hurufat numune kitabı
+                        oluşturmak üzere bir yazı galerisini alarak karıştırdığı
+                        1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.
+                        Beşyüz yıl boyunca varlığını sürdürmekle kalmamış,
+                        aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda
+                        Lorem Ipsum pasajları da içeren Letraset yapraklarının
+                        yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren
+                        masaüstü yayıncılık yazılımları ile popüler olmuştur.
+                        Neden Kullanırız?
+                    </p>
                 </div>
             </div>
             <div className='absolute w-full bottom-0 p-5'>
-                <div className='w-full p-1 rounded-full border items-center flex'>
+                <div className='w-full p-1 z-10 rounded-full border items-center flex'>
                     <IconButton>
                         <SentimentSatisfiedAltOutlinedIcon />
                     </IconButton>
