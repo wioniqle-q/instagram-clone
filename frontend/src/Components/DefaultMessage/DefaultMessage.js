56a2148ca0bed465ac7a21b28889d65918c7d296
batuhanbaybas <b.baybas@gmail.com>
2022-02-19 21:18:39 +0300

    updatate messge oage

---
+++ frontend/src/Components/DefaultMessage/DefaultMessage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DefaultMessage = () => {
+    return (
+        <div className='flex h-full flex-col justify-center items-center text-2xl'>
+            <div className='mb-5'>
+                Mesajlarınız
+            </div>
+            <div>
+                Arkadaşlarınla mesaj veya video paylaşın
+            </div>
+        </div>
+    )
+}
+
+export default DefaultMessage
---
+++ frontend/src/Components/UserTile/UserTile.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const UserTile = () => {
+    return (
+        <div className='w-full p-3 cursor-pointer hover:bg-gray-50'>
+            <div className='flex items-center'>
+                <img src='https://cdn.pixabay.com/photo/2021/02/27/16/25/woman-6055084__340.jpg' className='w-16 h-16 rounded-full' alt='profil resmi' />
+                <div className='flex flex-col text-sm px-2'>
+                    <span>
+                        Kullanıcı Adı
+                    </span>
+                    <span>
+                        son mesajo
+                    </span>
+
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default UserTile
