6bccfd7eb1d81255cee13c62a313bcae7d60a632
batuhanbaybas <b.baybas@gmail.com>
2022-02-20 15:03:54 +0300

    explore page added

--- frontend/src/Components/Explore/Explore.js
+++ frontend/src/Components/Explore/Explore.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const Explore = () => {
-  return (
-    <div>Explore</div>
-  )
+    return (
+        <div className={"w-full h-[90vh] mt-5 overflow-auto"}>
+            <div className={"flex  flex-wrap w-full h-1/5 justify-start"}>
+                <div className={"w-1/4 h-full p-1 "}>
+                    <img alt='resim' src={"https://cdn.pixabay.com/photo/2021/09/08/16/08/frog-6607232__340.jpg"}
+                        className={"w-full h-full"} />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Explore
--- frontend/src/Components/Messanger/Messanger.js
+++ frontend/src/Components/Messanger/Messanger.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import SentimentSatisfiedAltOutlinedIcon from '@mui/icons-material/SentimentSatisfiedAltOutlined';
-import {IconButton} from '@mui/material';
+import { IconButton } from '@mui/material';
 
 const Messanger = () => {
     return (<div className='w-full'>
@@ -8,7 +8,7 @@ const Messanger = () => {
             <div className='flex '>
                 <div>
                     <img src='https://picsum.photos/200/300' className='w-11 h-11 object-cover rounded-full'
-                         alt=''/>
+                        alt='' />
                 </div>
                 <span className='text-xs font-semibold px-2'>kullanıcı Adı </span>
             </div>
@@ -28,28 +28,13 @@ const Messanger = () => {
                     Neden Kullanırız?
                 </p>
             </div>
-            <div className='px-5 mb-3 w-2/3 float-left'>
-                <p className="border rounded-xl p-3 flex flex-start">
-                    Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
-                    metinlerdir. Lorem Ipsum,
-                    adı bilinmeyen bir matbaacının bir hurufat numune kitabı
-                    oluşturmak üzere bir yazı galerisini alarak karıştırdığı
-                    1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.
-                    Beşyüz yıl boyunca varlığını sürdürmekle kalmamış,
-                    aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda
-                    Lorem Ipsum pasajları da içeren Letraset yapraklarının
-                    yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren
-                    masaüstü yayıncılık yazılımları ile popüler olmuştur.
-                    Neden Kullanırız?
-                </p>
-            </div>
         </div>
         <div className='absolute w-full bottom-0 p-5'>
             <div className='w-full p-1 z-10 rounded-full border items-center flex'>
                 <IconButton>
-                    <SentimentSatisfiedAltOutlinedIcon/>
+                    <SentimentSatisfiedAltOutlinedIcon />
                 </IconButton>
-                <input className='outline-none' placeholder='Mesaj yaz...'/>
+                <input className='outline-none' placeholder='Mesaj yaz...' />
             </div>
         </div>
 
